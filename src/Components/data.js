4703e2bc2e2bd6f0c585bbd97da8e4738295172f
Stack Soumik <110479389+Soum-ik@users.noreply.github.com>
2023-12-22 23:41:14 +0600

    test: backend data from mongodb data base

---
+++ src/Components/data.js
@@ -0,0 +1,354 @@
+// card data
+export const data = [
+  {
+    id: 1,
+    title: "ArtCrypto",
+    img: "/collection4.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Celebrities",
+  },
+
+  {
+    id: 2,
+    title: "This NFT",
+    img: "/collection3.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Celebrities",
+  },
+
+  {
+    id: 3,
+    title: "NameCrypto",
+    img: "/collection2.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Sport",
+  },
+
+  {
+    id: 4,
+    title: "KingCrypto",
+    img: "/collection1.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Sport",
+  },
+
+  {
+    id: 5,
+    title: "ArtCrypto",
+    img: "/collection4.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Sport",
+  },
+
+  {
+    id: 6,
+    title: "This NFT",
+    img: "/collection3.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Crypto",
+  },
+
+  {
+    id: 7,
+    title: "NameCrypto",
+    img: "/collection2.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Crypto",
+  },
+
+  {
+    id: 8,
+    title: "KingCrypto",
+    img: "/collection1.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Crypto",
+  },
+
+  {
+    id: 9,
+    title: "ArtCrypto",
+    img: "/collection4.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Crypto",
+  },
+
+  {
+    id: 10,
+    title: "This NFT",
+    img: "/collection3.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Music",
+  },
+
+  {
+    id: 11,
+    title: "NameCrypto",
+    img: "/collection2.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Music",
+  },
+
+  {
+    id: 12,
+    title: "KingCrypto",
+    img: "/collection1.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Gaming",
+  },
+
+  {
+    id: 13,
+    title: "ArtCrypto",
+    img: "/collection4.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Gaming",
+  },
+
+  {
+    id: 14,
+    title: "This NFT",
+    img: "/collection3.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Celebrities",
+  },
+
+  {
+    id: 15,
+    title: "NameCrypto",
+    img: "/collection2.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Celebrities",
+  },
+
+  {
+    id: 16,
+    title: "KingCrypto",
+    img: "/collection1.png",
+    avatar: [
+      {
+        id: 1,
+        image: "/discover1.png",
+      },
+
+      {
+        id: 2,
+        image: "/discover2.png",
+      },
+      {
+        id: 3,
+        image: "/discover3.png",
+      },
+    ],
+    category: "Art",
+  },
+];
